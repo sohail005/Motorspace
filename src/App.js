3d52c41a46ab466d772def6fce3eb3f7a5abb3a2
jaweedkhan-espirittech <jaweedkhan@espirittech.com>
2025-07-15 15:38:13 +0530

    safearea issue ficed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Provider, useSelector } from 'react-redux';
 import { store } from './redux/store';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import RootNavigator from './navigation/RootNavigator';
 import { AppColors } from './constants/colors';
-// import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 const AppWrapper = () => {
   const statusBarColor = useSelector((state) => state.user.statusBarColor);
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: statusBarColor || AppColors.white }]}>
+    <SafeAreaView
+      edges={['top', 'left', 'right']}
+      style={[styles.container, { backgroundColor: statusBarColor || AppColors.white }]}
+    >
       <RootNavigator />
     </SafeAreaView>
   );
@@ -18,7 +21,9 @@ const AppWrapper = () => {
 const App = () => {
   return (
     <Provider store={store}>
-      <AppWrapper />
+      <SafeAreaProvider>
+        <AppWrapper />
+      </SafeAreaProvider>
     </Provider>
   );
 };
