4cad53d6bb8ac4c692c5dfa9a8556698d96a8832
sohail <sohail.mohammed@espirittech.com>
2025-07-15 21:22:21 +0530

    token check olgic fom storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Provider, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import { store } from './redux/store';
 import { StyleSheet } from 'react-native';
 import RootNavigator from './navigation/RootNavigator';
 import { AppColors } from './constants/colors';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import storageItem from './utils/storageItem';
+import { setToken } from './redux/features/user/userSlice';
 
 const AppWrapper = () => {
   const statusBarColor = useSelector((state) => state.user.statusBarColor);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadToken = async () => {
+      const token = await storageItem.getItem('token');
+      console.log("token::::::::::::::",token);
+      if (token) dispatch(setToken(token));
+    };
+    loadToken();
+  }, []);
   return (
     <SafeAreaView
       edges={['top', 'left', 'right']}
